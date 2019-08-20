8c1107a4afa8c2adcc6d369d40c66b0faf013a04
Julio Soto <jsberlanga@gmail.com>
2019-08-20 15:40:34 +0200

    restructure frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Signup from "./Signup";
+import Signin from "./Signin";
+import Signout from "./Signout";
+import CreateMessage from "./CreateMessage";
+import MessageList from "./MessageList/MessageList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Signup title="Signup" />
+      <Signin title="Signin" />
+      <Signout title="Signout" />
+      <CreateMessage title="CreateMessage" />
+      <MessageList title="Message List" />
+    </div>
+  );
+};
+
+export default App;
