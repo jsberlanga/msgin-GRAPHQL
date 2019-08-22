2e7a8939071843af1057809903c907ac4548baf5
Julio Soto <jsberlanga@gmail.com>
2019-08-22 18:42:15 +0200

    commit before apollo boost migration to support subscriptions

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,36 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 
+import { WebSocketLink } from "apollo-link-ws";
+import { HttpLink } from "apollo-link-http";
+import { split, from } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/__graphql"
+});
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
   uri: "http://localhost:4000/__graphql",
   credentials: "include"
