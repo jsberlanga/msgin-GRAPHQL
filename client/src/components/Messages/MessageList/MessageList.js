7fbbf96287da1f820bcdb2fd1c7a25c2c60e8c38
Julio Soto <jsberlanga@gmail.com>
2019-08-24 21:38:19 +0200

    feat(messagelist) add basic pagination

--- client/src/components/Messages/MessageList/MessageList.js
+++ client/src/components/Messages/MessageList/MessageList.js
@@ -5,8 +5,8 @@ import { useQuery } from "@apollo/react-hooks";
 import Message from "./Message";
 
 const GET_MESSAGES_QUERY = gql`
-  query GET_MESSAGES_QUERY {
-    getMessages {
+  query GET_MESSAGES_QUERY($skip: Int, $first: Int) {
+    getMessages(skip: $skip, first: $first) {
       id
       title
       body
@@ -22,23 +22,50 @@ const GET_MESSAGES_QUERY = gql`
 `;
 
 const MessageList = props => {
-  const { data, error, loading } = useQuery(GET_MESSAGES_QUERY);
-
+  const { data, error, loading, fetchMore } = useQuery(GET_MESSAGES_QUERY, {
+    variables: {
+      skip: 0,
+      first: 4
+    },
+    fetchPolicy: "cache-and-network"
+  });
   if (loading) return <div className="lds-dual-ring" />;
-  if (error) return `Error! `;
-
+  if (error) return `There was an error.`;
   return (
     <>
       <h1>The Latest Messages</h1>
-      <div className="message-list">
-        {data &&
-          data.getMessages.map(message => (
-            <Message key={message.id} message={message} />
-          ))}
+      <div className="messagelist__container">
+        <div className="messagelist">
+          {data &&
+            data.getMessages.map(message => (
+              <Message key={message.id} message={message} />
+            ))}
+        </div>
+        <button
+          className="messagelist__more"
+          onClick={() => {
+            fetchMore({
+              variables: {
+                skip: data.getMessages.length
+              },
+              updateQuery: (prev, { fetchMoreResult }) => {
+                if (!fetchMoreResult) return prev;
+                const obj = Object.assign({}, prev, {
+                  getMessages: [
+                    ...prev.getMessages,
+                    ...fetchMoreResult.getMessages
+                  ]
+                });
+                return obj;
+              }
+            });
+          }}
+        >
+          more...
+        </button>
       </div>
     </>
   );
 };
-
 export default MessageList;
 export { GET_MESSAGES_QUERY };
