86d47d2cf593aa37c86de181fc199b62dad045f3
Julio Soto <jsberlanga@gmail.com>
2019-08-25 22:24:09 +0200

    test(messagelist) add test for messagelist

---
+++ client/src/__tests__/MessageList.tests.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, waitForElement } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { MockedProvider } from "@apollo/react-testing";
+import MessageList from "../components/Messages/MessageList/MessageList";
+import { BrowserRouter } from "react-router-dom";
+import { GET_MESSAGES_MOCK } from "../__mocks__";
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
+
+test("should render messagelist", async () => {
+  const { getByTestId, getAllByTestId } = render(
+    <BrowserRouter>
+      <MockedProvider mocks={GET_MESSAGES_MOCK} addTypename={false}>
+        <MessageList />
+      </MockedProvider>
+    </BrowserRouter>
+  );
+
+  expect(getByTestId("loading")).toBeTruthy();
+  await waitForElement(() => getByTestId("messagelist"));
+  expect(getAllByTestId("message")).toBeTruthy();
+  expect(getAllByTestId("message-title")[0]).toHaveTextContent("Title 1");
+  expect(getAllByTestId("message-body")[1]).toHaveTextContent("Body 2");
+  expect(getAllByTestId("message-comments")).toHaveLength(1);
+});
