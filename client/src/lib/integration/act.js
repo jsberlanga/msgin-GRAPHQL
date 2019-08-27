f7393b899c27e5d564942f9ff26deff101587524
Julio Soto <jsberlanga@gmail.com>
2019-08-27 10:20:31 +0200

    refactor(test) add fixActBug to lib folder

---
+++ client/src/lib/integration/act.js
@@ -0,0 +1,17 @@
+const fixActWarning = () => {
+  const originalError = console.error;
+  beforeAll(() => {
+    console.error = (...args) => {
+      if (/Warning.*not wrapped in act/.test(args[0])) {
+        return;
+      }
+      originalError.call(console, ...args);
+    };
+  });
+
+  afterAll(() => {
+    console.error = originalError;
+  });
+};
+
+export default fixActWarning;
