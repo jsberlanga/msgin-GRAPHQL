9711d4186d86689a8ae7f668d35fa6ab91d86c06
Julio Soto <jsberlanga@gmail.com>
2019-08-27 11:15:02 +0200

    feat(home) add comment from owner

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import homeImage from "../../assets/images/main.jpg";
+import owner from "../../assets/images/ned.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>We give you the good news!</h1>
-      <img className="home--image" src={homeImage} alt="main" />
+    <div className="home--container">
+      <h1 className="home--title">We only give you the good news!</h1>
+      <div className="home--image">
+        <div className="home--image__main--container">
+          <img
+            className="home--image__main--image"
+            src={homeImage}
+            alt="main"
+          />
+        </div>
+        <div className="home--image__rest--container">
+          <h2>A word from our lord and savior</h2>
+          <p>There are some things we don't want to know. Important things.</p>
+          <img className="home--image__rest--image" src={owner} alt="random1" />
+        </div>
+      </div>
     </div>
   );
 };
